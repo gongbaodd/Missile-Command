5a59d38e9f70279a2f662ad489c95c4bae6fca2a
gongbaodd@ProArt <gongbaodd@qq.com>
2024-11-14 18:11:00 +0200

    Make canvas responsive and add window resize handling

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
 }
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// 🧨🕹️🧪🔭🏹🗡️🔪🔫✏️🖌️🖋️🖍️🖊️🚀🛸⚓🚨🚩🏴🏳️🏠🏚️🏡🏛️🕌🏤🏢🏣🏦🏪🏩🏥🏨🏭💒🏰🏬🏫🏯🗼🪠🔥
