6c949e0b6d759132df9b961ccafa3156736786da
gongbaodd@ProArt <gongbaodd@qq.com>
2024-11-29 13:12:49 +0200

    cursor hover

--- sketch.js
+++ sketch.js
@@ -39,6 +39,7 @@ class Cursor {
     this.space = space
   }
 }
+Cursor.mousePos = null
 
 class Color {
   constructor(r, g, b) {
@@ -47,6 +48,8 @@ class Color {
     this.b = b
   }
 }
+
+class HoverColor extends Color {}
 
 const IDs = {
   "ground": "ground",
@@ -69,7 +72,7 @@ class System {
         fill(color.r, color.g, color.b)
         cylinder(ground.radius, ground.height)
         pop()
-      }
+      }// Ground
 
       if (entity.getComponent(Cursor)) {
         const ground = this.entities.find(entity => entity.id === IDs.ground)
@@ -77,20 +80,41 @@ class System {
         const groundShape = ground.getComponent(Ground)
         const cursor = entity.getComponent(Cursor)
         const color = entity.getComponent(Color)
+        const hoverColor = entity.getComponent(HoverColor)
+
+        addCursorDebugger()
 
         for (let x = -groundShape.radius; x < groundShape.radius; x += cursor.space) {
           for (let z = -groundShape.radius; z < groundShape.radius; z += cursor.space) {
             if (x * x + z * z <= groundShape.radius * groundShape.radius) {
+              // if in the ground
+              const v = createVector(x, groundPos.y, z)
+              const { dist } =  {
+                get dist() {
+                  if (!Cursor.mousePos) {
+                    return Infinity
+                  }
+                  return v.dist(Cursor.mousePos)
+                }
+              }
+
               push()
-              translate(x, groundPos.y, z)
+              translate(v.x, v.y, v.z)
+
               fill(color.r, color.g, color.b)
+
+              if (dist < cursor.radius) {
+                fill(hoverColor.r, hoverColor.g, hoverColor.b)
+              }
+
               sphere(cursor.radius)
               pop()
             }
           }
         }
+      } // Cursor
 
-      }
+
     });
   }
 }
@@ -112,9 +136,10 @@ function setup() {
   addGroundMenu(ground)
 
   const cursor = new Entity(IDs.cursor)
-  cursor.addComponent(new Cursor(5, 33))
+  cursor.addComponent(new Cursor(6, 40))
   cursor.addComponent(new Position(0, 300, 0))
   cursor.addComponent(new Color(0, 0, 255))
+  cursor.addComponent(new HoverColor(255, 0, 0))
   entities.push(cursor)
   addCursorMenu(cursor)
 
@@ -139,9 +164,31 @@ function drawScene() {
   system.update()
 }
 
-function mousePressed() {
- 
-  
+function mouseMoved() {
+  // the default fovy is 2 * atan(height / 2 / 800). 800 is the camera's Z position
+  const ground = system.entities.find(e => e.getComponent(Ground))
+  const gPos = ground.getComponent(Position)
+  const planeY = gPos.y
+
+  let mouseX3D = (mouseX - width / 2) / (width / 2)
+  let mouseY3D = (mouseY - height / 2) / (height / 2)
+
+  let fov = PI / 3
+  let aspect = width / height
+  let nearPlane = tan(fov / 2)
+
+  let rayDir = createVector(
+    mouseX3D * nearPlane * aspect,
+    mouseY3D * nearPlane,
+    -1,
+  ).normalize()
+
+  let cameraPos = createVector(0, 0, 800)
+
+  let t = (planeY - cameraPos.y) / rayDir.y
+
+  const mousePos = p5.Vector.add(cameraPos, p5.Vector.mult(rayDir, t))
+  Cursor.mousePos = mousePos
 }
 
 function addGroundMenu(ground) {
@@ -158,7 +205,7 @@ function addGroundMenu(ground) {
 }
 
 function addCursorMenu(cursor) {
-  if(DEV) {
+  if (DEV) {
     const cursorMenu = gui.addFolder("cursor")
     cursorMenu.add(cursor.getComponent(Cursor), "radius", 5, 10).name("Radius")
     cursorMenu.add(cursor.getComponent(Cursor), "space", 10, 50).name("Space")
@@ -166,5 +213,23 @@ function addCursorMenu(cursor) {
     colorMenu.add(cursor.getComponent(Color), "r", 0, 255).name("R")
     colorMenu.add(cursor.getComponent(Color), "g", 0, 255).name("G")
     colorMenu.add(cursor.getComponent(Color), "b", 0, 255).name("B")
+    const hoverMenu = cursorMenu.addFolder("hover color")
+    hoverMenu.add(cursor.getComponent(HoverColor), "r", 0, 255).name("R")
+    hoverMenu.add(cursor.getComponent(HoverColor), "g", 0, 255).name("G")
+    hoverMenu.add(cursor.getComponent(HoverColor), "b", 0, 255).name("B")
+  }
+}
+
+function addCursorDebugger() {
+  if (DEV) {
+    if (Cursor.mousePos) {
+      push()
+
+      translate(Cursor.mousePos.x, Cursor.mousePos.y, Cursor.mousePos.z)
+      fill(0, 255, 0)
+      sphere(5)
+
+      pop()
+    }
   }
 }
