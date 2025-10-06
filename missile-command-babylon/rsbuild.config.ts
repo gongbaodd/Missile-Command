f5cfb5c6e4b9025d1d6f9aa2c6c7e77846e79d6a
gongbaodd@ProArt <gongbaodd@qq.com>
2025-10-06 15:30:20 +0300

    install solid-js

--- missile-command-babylon/rsbuild.config.ts
+++ missile-command-babylon/rsbuild.config.ts
@@ -1,3 +1,12 @@
 import { defineConfig } from '@rsbuild/core';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+import { pluginSolid } from '@rsbuild/plugin-solid';
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/,
+    }),
+    pluginSolid(),
+  ],
+});
