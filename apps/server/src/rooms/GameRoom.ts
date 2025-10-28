05e24f359487215963b456158ef53badb20221ee
gongbaodd@ProArt <gongbaodd@qq.com>
2025-10-28 11:44:13 +0200

    setup player states

--- apps/server/src/rooms/GameRoom.ts
+++ apps/server/src/rooms/GameRoom.ts
@@ -1,10 +1,21 @@
 import { Room, Client } from "colyseus";
 import { Schema, type } from "@colyseus/schema";
+import { generateUsername } from "unique-username-generator";
+
+enum PlayerRole {
+	ATTACKER = 'attacker',
+	DEFENDER = 'defender'
+}
 
 class Player extends Schema {
 	@type("string") id: string = "";
 	@type("string") name: string = "";
 	@type("number") score: number = 0;
+	@type("string") role: PlayerRole = PlayerRole.DEFENDER;
+}
+
+interface IOnJoinOptions {
+	role?: PlayerRole;
 }
 
 class GameState extends Schema {
@@ -12,19 +23,19 @@ class GameState extends Schema {
 }
 
 export class GameRoom extends Room<GameState> {
+    state = new GameState();
 	onCreate() {
-		this.setState(new GameState());
-
 		this.onMessage("message", (client, message) => {
             console.log("ChatRoom received message from", client.sessionId, ":", message);
             this.broadcast("messages", `(${client.sessionId}) ${message}`);
         });
 	}
 
-	onJoin(client: Client, options: any) {
+	onJoin(client: Client, options: IOnJoinOptions) {
 		const player = new Player();
 		player.id = client.sessionId;
-		player.name = options?.name ?? "Player";
+		player.name = generateUsername("-", 2, 20);
+		player.role = options.role ?? PlayerRole.DEFENDER;
 		this.state.players.push(player);
 
 		this.broadcast("messages", `${ client.sessionId } joined.`);
